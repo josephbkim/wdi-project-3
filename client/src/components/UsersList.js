3a2fb4ffa16b5341bd8e09102176f4ced5328ff7
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-01-24 13:11:31 -0500

    Added garden details to specific garden page

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -30,22 +30,44 @@ class UsersList extends Component {
 
 
     render() {
-        
+
         const gardenId = this.state.garden._id
         const userList = this.state.garden.users.map((user, index) => {
-                return (<div key={index}>
-                    <div>
-                        <Link to={`/gardens/${gardenId}/users/${user._id}`}>{user.firstName}</Link>
-                    </div>
-                    <div>{user.email}</div>
-                        </div>)
+            return (<div key={index}>
+                <div>
+                    <Link to={`/gardens/${gardenId}/users/${user._id}`}>{user.firstName}</Link>
+                </div>
+                <div>{user.email}</div>
+            </div>)
         })
+        const userCount = this.state.garden.users.length
+
+        const GardenDiv = styled.div`
+        border: 1px solid black;
+        margin: 20px;
+        padding: 20px;
+        `
+        const UserList = styled.div`
+        border: 3px solid brown;
+        border-radius: 5px;
+        margin: 20px;
+        padding: 20px;
+        `
 
         return (
             <div>
-                <div>{this.state.garden.name}</div>
-                <div>{this.state.garden.address}</div>
-                <div>{userList}</div>
+                <GardenDiv className='garden-detail'>
+                    <div>{this.state.garden.name}</div>
+                    <div>Addres:</div>
+                    <div>{this.state.garden.address}</div>
+                    <div>{this.state.garden.city}, {this.state.garden.state}</div>
+                    <div>Number of Gardeners: {userCount}</div>
+                    <div>Number of Available: {10 - userCount}</div>
+                    <div>Cost of Plot: </div>
+                </GardenDiv>
+                <div>
+                    {userList}
+                </div>
             </div>
         )
     }
